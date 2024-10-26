1c81dba34c00efc5b91cf5ca955c63da2c788cc9
Jo Carrasco <jogregcarrasco@gmail.com>
2024-10-25 20:08:33 -0400

    fix: add commands for build and remove other files

--- src/index.ts
+++ src/index.ts
@@ -10,11 +10,11 @@ import 'dotenv/config';
     driver = await new Builder().forBrowser(Browser.CHROME).build();
     const username = process.env.BDV_USERNAME;
     const password = process.env.BDV_PASSWORD;
-    console.log(username, password);
     if (!username || !password) {
       console.error("Please provide a username and password in the ENV file");
       return;
     }
+    
     const loginSuccessfully = await login(driver, username, password);
     if (loginSuccessfully) {
       // Do operations
@@ -23,7 +23,6 @@ import 'dotenv/config';
       await logout(driver);
     }
   } catch (e: any) {
-
     console.error(e);
   }
 })();
